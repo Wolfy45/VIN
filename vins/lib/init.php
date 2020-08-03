481b5dc4ab70c5a5bd591d53974449947f3104f8
Wolfy45 <68100369+Wolfy45@users.noreply.github.com>
2020-08-03 15:02:44 +0200

    Update init.php

--- vins/lib/init.php
+++ vins/lib/init.php
@@ -1,23 +1,2 @@
 <?php
 
-/* 
- * Toutes les actions d'initialisation des controleurs
- *
- */
-
-
-// Affichage des messages d'erreur
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
-
-// 2) Inclure les librairies de fonctions (chargement des fonctions)
-
-
-// 3) Inclure les classes
-include "classes/vin.php";
-include "classes/utilisateur_vin";
-
-// 4) Ouvrir la base de données
-global $bdd;        // Crée ou récupère la variable globale 
-$bdd = new PDO("mysql:host=localhost;dbname=bdd_name;charset=UTF8", "login", "password");
-$bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);        // Que en phase de développement
